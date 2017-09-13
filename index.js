4a5eb798ddb3bc55db78999e77e1ba624f26f177
Anthony deguzman110@gmail.com <deguzman110@gmail.com>
2017-09-13 15:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
 
-  for (i = 0; i < musicians.length; i++) {
+  for (musicians = 0; musicians < instruments.length; musicians++) {
     newArray.push("${muscians} plays the ${instruments}");
   }
   return newArray;
